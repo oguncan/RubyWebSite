e522210c926c9af6ec861ac682d68e7c00aad6e8
Ogun <oguncanlnx@gmail.com>
2019-04-18 22:15:07 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :models
+  get 'about', to: 'model#about'
   root 'models#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
